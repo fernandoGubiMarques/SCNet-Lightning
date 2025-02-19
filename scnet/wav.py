83bd95d424eab68da9b23841d416e2f7b64a85cd
Fernando Marques <fernandogubitosom@gmail.com>
2025-02-19 12:48:30 -0300

    complete implementation

--- scnet/wav.py
+++ scnet/wav.py
@@ -11,85 +11,6 @@ from tqdm import tqdm
 from typing import Literal
 import torchaudio
 from torch.nn import functional as F
-import math
-import json
-import julius
-from .utils import convert_audio_channels
-from collections import OrderedDict
-
-# MIXTURE = "mixture"
-# EXT = ".wav"
-
-
-# def _track_metadata(track, sources, normalize=True, ext=EXT):
-#     track_length = None
-#     track_samplerate = None
-#     mean = 0
-#     std = 1
-#     for source in sources + [MIXTURE]:
-#         file = track / f"{source}{ext}"
-#         try:
-#             info = torchaudio.info(str(file))
-#         except RuntimeError:
-#             print(file)
-#             raise
-#         length = info.num_frames
-#         if track_length is None:
-#             track_length = length
-#             track_samplerate = info.sample_rate
-#         elif track_length != length:
-#             raise ValueError(
-#                 f"Invalid length for file {file}: "
-#                 f"expecting {track_length} but got {length}."
-#             )
-#         elif info.sample_rate != track_samplerate:
-#             raise ValueError(
-#                 f"Invalid sample rate for file {file}: "
-#                 f"expecting {track_samplerate} but got {info.sample_rate}."
-#             )
-#         if source == MIXTURE and normalize:
-#             try:
-#                 wav, _ = torchaudio.load(str(file))
-#             except RuntimeError:
-#                 print(file)
-#                 raise
-#             wav = wav.mean(0)
-#             mean = wav.mean().item()
-#             std = wav.std().item()
-
-#     return {"length": length, "mean": mean, "std": std, "samplerate": track_samplerate}
-
-
-# def build_metadata(path, sources, normalize=True, ext=EXT):
-#     """
-#     Build the metadata for `Wavset`.
-
-#     Args:
-#         path (str or Path): path to dataset.
-#         sources (list[str]): list of sources to look for.
-#         normalize (bool): if True, loads full track and store normalization
-#             values based on the mixture file.
-#         ext (str): extension of audio files (default is .wav).
-#     """
-
-#     meta = {}
-#     path = Path(path)
-#     pendings = []
-#     from concurrent.futures import ThreadPoolExecutor
-
-#     with ThreadPoolExecutor(8) as pool:
-#         for root, folders, files in os.walk(path, followlinks=True):
-#             root = Path(root)
-#             if root.name.startswith(".") or folders or root == path:
-#                 continue
-#             name = str(root.relative_to(path))
-#             pendings.append(
-#                 (name, pool.submit(_track_metadata, root, sources, normalize, ext))
-#             )
-#             # meta[name] = _track_metadata(root, sources, normalize, ext)
-#         for name, pending in tqdm.tqdm(pendings, ncols=120):
-#             meta[name] = pending.result()
-#     return meta
 
 
 class Wavset(Dataset):
@@ -108,15 +29,15 @@ class Wavset(Dataset):
     ):
         self.other_stem = other_stem
         self.stems = stems
-        self.ignore_stems = ignore_stems
+        self.ignore_stems = ignore_stems if isinstance(ignore_stems, list) else [ignore_stems]
         self.extension = extension
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        segment_samples = int(segment * samplerate) if segment else -1
+        self.segment_samples = int(segment * samplerate) if segment else -1
         stride_samples = int(stride * samplerate)
 
         assert Path(partitioning_path).exists()
         with open(partitioning_path) as f:
-            partitioning = yaml.load(f)
+            partitioning = yaml.safe_load(f)
 
         root = Path(partitioning["root"])
 
@@ -132,25 +53,36 @@ class Wavset(Dataset):
             for stem in stems:
                 assert (song_dir / f"{stem}.{extension}").exists()
 
-            # Load example stem
-            audio, _ = torchaudio.load(song_dir / f"{stems[0]}.{extension}")
-
             # Compute stats
+            audio = []
+            for file in song_dir.glob(f"*.{self.extension}"):
+                if file.stem in ignore_stems:
+                    continue
+                stem_audio, sr = torchaudio.load(file)
+                assert sr == samplerate
+                audio.append(stem_audio.to(self.device))
+            
+            audio = sum(audio)
+            mean = audio.mean()
+            std = audio.std()
+            
             audio_len = audio.shape[-1]
-            if segment_samples > 0:
-                segment_starts = list(range(0, audio_len - segment_samples, stride_samples))
+            if self.segment_samples > 0 and audio_len >= self.segment_samples:
+                segment_starts = list(range(0, audio_len - self.segment_samples, stride_samples))
             else:
                 segment_starts = [0]
-            segment_durations = [segment_samples] * len(segment_starts)
+            segment_durations = [self.segment_samples] * len(segment_starts)
             dirs = [song_dir] * len(segment_starts)
+            means = [mean] * len(segment_starts)
+            stds = [std] * len(segment_starts)
 
-            self.metadata += list(zip(segment_starts, segment_durations, dirs))
+            self.metadata += list(zip(dirs, segment_starts, segment_durations, means, stds))
         
     def __len__(self):
         return len(self.metadata)
     
     def __getitem__(self, i):
-        start, duration, dir = self.metadata[i]
+        dir, start, duration, mean, std = self.metadata[i]
         dir: Path
 
         stems = {s: 0 for s in self.stems}
@@ -164,113 +96,18 @@ class Wavset(Dataset):
             a = a.to(self.device)
             if file.stem in self.stems:
                 stems[file.stem] = a
-            else:
+            elif self.other_stem:
                 stems["other"] += a
         
-        return torch.stack(...)
-
-
-# class Wavset:
-#     def __init__(
-#             self,
-#             root, metadata, sources,
-#             segment=None, shift=None, normalize=True,
-#             samplerate=44100, channels=2, ext=EXT):
-#         """
-#         Waveset (or mp3 set for that matter). Can be used to train
-#         with arbitrary sources. Each track should be one folder inside of `path`.
-#         The folder should contain files named `{source}.{ext}`.
-
-#         Args:
-#             root (Path or str): root folder for the dataset.
-#             metadata (dict): output from `build_metadata`.
-#             sources (list[str]): list of source names.
-#             segment (None or float): segment length in seconds. If `None`, returns entire tracks.
-#             shift (None or float): stride in seconds bewteen samples.
-#             normalize (bool): normalizes input audio, **based on the metadata content**,
-#                 i.e. the entire track is normalized, not individual extracts.
-#             samplerate (int): target sample rate. if the file sample rate
-#                 is different, it will be resampled on the fly.
-#             channels (int): target nb of channels. if different, will be
-#                 changed onthe fly.
-#             ext (str): extension for audio files (default is .wav).
-
-#         samplerate and channels are converted on the fly.
-#         """
-#         self.root = Path(root)
-#         self.metadata = OrderedDict(metadata)
-#         self.segment = segment
-#         self.shift = shift or segment
-#         self.normalize = normalize
-#         self.sources = sources
-#         self.channels = channels
-#         self.samplerate = samplerate
-#         self.ext = ext
-#         self.num_examples = []
-#         for name, meta in self.metadata.items():
-#             track_duration = meta['length'] / meta['samplerate']
-#             if segment is None or track_duration < segment:
-#                 examples = 1
-#             else:
-#                 examples = int(math.ceil((track_duration - self.segment) / self.shift) + 1)
-#             self.num_examples.append(examples)
-
-#     def __len__(self):
-#         return sum(self.num_examples)
-
-#     def get_file(self, name, source):
-#         return self.root / name / f"{source}{self.ext}"
-
-#     def __getitem__(self, index):
-#         for name, examples in zip(self.metadata, self.num_examples):
-#             if index >= examples:
-#                 index -= examples
-#                 continue
-#             meta = self.metadata[name]
-#             num_frames = -1
-#             offset = 0
-#             if self.segment is not None:
-#                 offset = int(meta['samplerate'] * self.shift * index)
-#                 num_frames = int(math.ceil(meta['samplerate'] * self.segment))
-#             wavs = []
-#             for source in self.sources:
-#                 file = self.get_file(name, source)
-#                 wav, _ = torchaudio.load(str(file), frame_offset=offset, num_frames=num_frames)
-#                 wav = convert_audio_channels(wav, self.channels)
-#                 wavs.append(wav)
-
-#             example = torch.stack(wavs)
-#             example = julius.resample_frac(example, meta['samplerate'], self.samplerate)
-#             if self.normalize:
-#                 example = (example - meta['mean']) / meta['std']
-#             if self.segment:
-#                 length = int(self.segment * self.samplerate)
-#                 example = example[..., :length]
-#                 example = F.pad(example, (0, length - example.shape[-1]))
-#             return example, meta['mean'], meta['std']
-
-
-# def get_wav_datasets(args):
-#     """Extract the wav datasets from the XP arguments."""
-#     sig = hashlib.sha1(str(args.wav).encode()).hexdigest()[:8]
-#     metadata_file = Path(args.metadata) / ('wav_' + sig + ".json")
-#     train_path = Path(args.wav) / "train"
-#     valid_path = Path(args.wav) / "valid"
-#     if not metadata_file.is_file() and accelerator.is_main_process:
-#         metadata_file.parent.mkdir(exist_ok=True, parents=True)
-#         train = build_metadata(train_path, args.sources)
-#         valid = build_metadata(valid_path, args.sources)
-#         json.dump([train, valid], open(metadata_file, "w"))
-#     accelerator.wait_for_everyone()
-
-#     train, valid = json.load(open(metadata_file))
-#     kw_cv = {}
-
-#     train_set = Wavset(train_path, train, args.sources,
-#                        segment=args.segment, shift=args.shift,
-#                        samplerate=args.samplerate, channels=args.channels,
-#                        normalize=args.normalize)
-#     valid_set = Wavset(valid_path, valid, [MIXTURE] + list(args.sources),
-#                        samplerate=args.samplerate, channels=args.channels,
-#                        normalize=args.normalize, **kw_cv)
-#     return train_set, valid_set
+        if self.other_stem:
+            r =  torch.stack([stems[s] for s in self.stems] + [stems["other"]])
+        else:
+            r = torch.stack([stems[s] for s in self.stems])
+        
+        if r.shape[-1] < self.segment_samples and self.segment_samples > 0:
+            r = F.pad(r, (0, self.segment_samples - r.shape[-1]))
+        
+        r -= mean
+        r /= std
+        
+        return r, mean, std
