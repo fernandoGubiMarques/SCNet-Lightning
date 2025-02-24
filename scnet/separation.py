1c05b5fadf3d631b3025621e84d3adbf6c208ddd
Fernando Marques <fernandogubitosom@gmail.com>
2025-02-24 13:06:19 -0300

    use einops in dual path transformer

--- scnet/separation.py
+++ scnet/separation.py
@@ -1,26 +1,29 @@
 import torch
 import torch.nn as nn
 from torch.nn.modules.rnn import LSTM
+import einops
+
 
 class FeatureConversion(nn.Module):
     """
-    Integrates into the adjacent Dual-Path layer. 
+    Integrates into the adjacent Dual-Path layer.
 
     Args:
         channels (int): Number of input channels.
         inverse (bool): If True, uses ifft; otherwise, uses rfft.
-    """    
+    """
+
     def __init__(self, channels, inverse):
         super().__init__()
         self.inverse = inverse
-        self.channels= channels
+        self.channels = channels
 
     def forward(self, x):
         # B, C, F, T = x.shape
         if self.inverse:
             x = x.float()
-            x_r = x[:, :self.channels//2, :, :]
-            x_i = x[:, self.channels//2:, :, :]
+            x_r = x[:, : self.channels // 2, :, :]
+            x_i = x[:, self.channels // 2 :, :, :]
             x = torch.complex(x_r, x_i)
             x = torch.fft.irfft(x, dim=3, norm="ortho")
         else:
@@ -33,7 +36,7 @@ class FeatureConversion(nn.Module):
 
 
 class DualPathRNN(nn.Module):
-    """  
+    """
     Dual-Path RNN in Separation Network.
 
     Args:
@@ -41,6 +44,7 @@ class DualPathRNN(nn.Module):
         expand (int): Expansion factor used to calculate the hidden_size of LSTM.
         bidirectional (bool): If True, becomes a bidirectional LSTM.
     """
+
     def __init__(self, d_model, expand, bidirectional=True):
         super(DualPathRNN, self).__init__()
 
@@ -48,21 +52,31 @@ class DualPathRNN(nn.Module):
         self.hidden_size = d_model * expand
         self.bidirectional = bidirectional
         # Initialize LSTM layers and normalization layers
-        self.lstm_layers = nn.ModuleList([self._init_lstm_layer(self.d_model, self.hidden_size) for _ in range(2)])
-        self.linear_layers = nn.ModuleList([nn.Linear(self.hidden_size*2, self.d_model) for _ in range(2)])
+        self.lstm_layers = nn.ModuleList(
+            [self._init_lstm_layer(self.d_model, self.hidden_size) for _ in range(2)]
+        )
+        self.linear_layers = nn.ModuleList(
+            [nn.Linear(self.hidden_size * 2, self.d_model) for _ in range(2)]
+        )
         self.norm_layers = nn.ModuleList([nn.GroupNorm(1, d_model) for _ in range(2)])
 
     def _init_lstm_layer(self, d_model, hidden_size):
-        return LSTM(d_model, hidden_size, num_layers=1, bidirectional=self.bidirectional, batch_first=True)
+        return LSTM(
+            d_model,
+            hidden_size,
+            num_layers=1,
+            bidirectional=self.bidirectional,
+            batch_first=True,
+        )
 
     def forward(self, x):
         B, C, F, T = x.shape
-        
+
         # Process dual-path rnn
         original_x = x
         # Frequency-path
         x = self.norm_layers[0](x)
-        x = x.transpose(1, 3).contiguous().view(B * T, F, C)  
+        x = x.transpose(1, 3).contiguous().view(B * T, F, C)
         x, _ = self.lstm_layers[0](x)
         x = self.linear_layers[0](x)
         x = x.view(B, T, F, C).transpose(1, 3)
@@ -83,28 +97,36 @@ class DualPathRNN(nn.Module):
 class SeparationNet(nn.Module):
     """
     Implements a simplified Sparse Down-sample block in an encoder architecture.
-    
+
     Args:
     - channels (int): Number input channels.
     - expand (int): Expansion factor used to calculate the hidden_size of LSTM.
     - num_layers (int): Number of dual-path layers.
     """
+
     def __init__(self, channels, expand=1, num_layers=6):
         super(SeparationNet, self).__init__()
-        
+
         self.num_layers = num_layers
 
-        self.dp_modules = nn.ModuleList([
-                DualPathRNN(channels * (2 if i % 2 == 1 else 1), expand) for i in range(num_layers)
-        ])
-        
-        self.feature_conversion = nn.ModuleList([
-            FeatureConversion(channels * 2 , inverse = False if i % 2 == 0 else True) for i in range(num_layers)
-        ])
+        self.dp_modules = nn.ModuleList(
+            [
+                DualPathRNN(channels * (2 if i % 2 == 1 else 1), expand)
+                for i in range(num_layers)
+            ]
+        )
+
+        self.feature_conversion = nn.ModuleList(
+            [
+                FeatureConversion(channels * 2, inverse=False if i % 2 == 0 else True)
+                for i in range(num_layers)
+            ]
+        )
+
     def forward(self, x):
         for i in range(self.num_layers):
-           x = self.dp_modules[i](x)
-           x = self.feature_conversion[i](x)
+            x = self.dp_modules[i](x)
+            x = self.feature_conversion[i](x)
         return x
 
 
@@ -150,22 +172,21 @@ class DualPathTransformer(nn.Module):
 
         # Frequency-path
         x = self.norm_layers[0](x)
-        x = x.permute(3, 0, 2, 1).contiguous().view(T, B * F, C)  # (T, B*F, C)
-        x = self.transformer_layers[0](
-            x
-        )  # Transformer operates on (seq_len, batch, features)
+        # Transformer operates on (seq_len, batch, features)
+        x = einops.rearrange(x, "B C F T -> T (B F) C")
+        x = self.transformer_layers[0](x)
         x = self.linear_layers[0](x)
-        x = x.view(T, B, F, C).permute(1, 3, 2, 0)  # (B, C, F, T)
+        x = einops.rearrange(x, "T (B F) C -> B C F T", B=B, F=F)
         x = x + original_x
 
         original_x = x
 
         # Time-path
         x = self.norm_layers[1](x)
-        x = x.permute(2, 0, 1, 3).contiguous().view(F, B * T, C)  # (F, B*T, C)
+        x = einops.rearrange(x, "B C F T -> F (B T) C")
         x = self.transformer_layers[1](x)
         x = self.linear_layers[1](x)
-        x = x.view(F, B, T, C).permute(1, 3, 0, 2)  # (B, C, F, T)
+        x = einops.rearrange(x, "F (B T) C -> B C F T", B=B, T=T)
         x = x + original_x
 
         return x
@@ -193,10 +214,12 @@ class SeparationTransformer(nn.Module):
             ]
         )
 
-        self.feat_conversion = nn.ModuleList([
-            FeatureConversion(channels * 2, inverse=(i % 2 != 0))
-            for i in range(num_layers)
-        ])
+        self.feat_conversion = nn.ModuleList(
+            [
+                FeatureConversion(channels * 2, inverse=(i % 2 != 0))
+                for i in range(num_layers)
+            ]
+        )
 
     def forward(self, x):
         for dp, feat_conv in zip(self.dp_modules, self.feat_conversion):
